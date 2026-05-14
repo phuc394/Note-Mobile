2ade9677dbf4cf90e73cfd342a85f5d048805cf7
phuc394 <phuc.thg02394@sinhvien.hoasen.edu.vn>
2026-05-14 21:08:14 +0700

    feat: Added Login and Register

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,6 @@
 import dotenv from "dotenv";
 import express from "express";
+import AuthRoute from "./routes/AuthRoute.js";
 
 dotenv.config();
 
@@ -9,5 +10,9 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 
+
+
+// Use routes
+app.use('/auth', AuthRoute);
 
 export default app;
--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
-import app from "./app";
+import app from "./app.js";
 
 const port = Number(process.env.PORT ?? 3000);
 
 app.listen(port, () => {
-  console.log(`ReportService listening on port ${port}`);
+  console.log(`Backend is listening on port ${port}`);
 });
